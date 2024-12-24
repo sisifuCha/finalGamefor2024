5563cb95ed6583b3e2428b96165d4bc93f4cc906
sisifuCha <xxfc21214@126.com>
2024-12-24 21:23:48 +0800

    1

---
+++ build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/debug/moc_cgamedlg.cpp
@@ -0,0 +1,310 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'cgamedlg.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.7.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../cgamedlg.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'cgamedlg.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.7.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSCGameDlgENDCLASS_t {};
+constexpr auto qt_meta_stringdata_CLASSCGameDlgENDCLASS = QtMocHelpers::stringData(
+    "CGameDlg",
+    "gameToMain",
+    "",
+    "gameToTheme",
+    "gameToHelp",
+    "on_btn_gameToMain_clicked",
+    "doMainToGame",
+    "doThemeToGame",
+    "on_btn_gameToTheme_clicked",
+    "do_theme_background_change",
+    "do_theme_gem_change",
+    "on_pushButton_stop_clicked",
+    "update_timebar",
+    "Game_start",
+    "on_pushButton_continue_clicked",
+    "on_pushButton_restart_clicked",
+    "do_btn_hint",
+    "paintEvent",
+    "QPaintEvent*",
+    "event",
+    "closeEvent",
+    "QCloseEvent*",
+    "on_pushButton_clicked",
+    "on_pushButton_2_clicked",
+    "on_pushButton_cross_clicked",
+    "on_pushButton_color_clicked",
+    "on_pushButton_boom_clicked",
+    "on_help_btn_clicked"
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSCGameDlgENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      23,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       3,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  152,    2, 0x06,    1 /* Public */,
+       3,    0,  153,    2, 0x06,    2 /* Public */,
+       4,    0,  154,    2, 0x06,    3 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       5,    0,  155,    2, 0x08,    4 /* Private */,
+       6,    0,  156,    2, 0x08,    5 /* Private */,
+       7,    0,  157,    2, 0x08,    6 /* Private */,
+       8,    0,  158,    2, 0x08,    7 /* Private */,
+       9,    1,  159,    2, 0x08,    8 /* Private */,
+      10,    1,  162,    2, 0x08,   10 /* Private */,
+      11,    0,  165,    2, 0x08,   12 /* Private */,
+      12,    0,  166,    2, 0x08,   13 /* Private */,
+      13,    0,  167,    2, 0x08,   14 /* Private */,
+      14,    0,  168,    2, 0x08,   15 /* Private */,
+      15,    0,  169,    2, 0x08,   16 /* Private */,
+      16,    0,  170,    2, 0x08,   17 /* Private */,
+      17,    1,  171,    2, 0x08,   18 /* Private */,
+      20,    1,  174,    2, 0x08,   20 /* Private */,
+      22,    0,  177,    2, 0x08,   22 /* Private */,
+      23,    0,  178,    2, 0x08,   23 /* Private */,
+      24,    0,  179,    2, 0x08,   24 /* Private */,
+      25,    0,  180,    2, 0x08,   25 /* Private */,
+      26,    0,  181,    2, 0x08,   26 /* Private */,
+      27,    0,  182,    2, 0x08,   27 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 18,   19,
+    QMetaType::Void, 0x80000000 | 21,   19,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject CGameDlg::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSCGameDlgENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSCGameDlgENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSCGameDlgENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<CGameDlg, std::true_type>,
+        // method 'gameToMain'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'gameToTheme'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'gameToHelp'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btn_gameToMain_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'doMainToGame'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'doThemeToGame'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btn_gameToTheme_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'do_theme_background_change'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        // method 'do_theme_gem_change'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        // method 'on_pushButton_stop_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'update_timebar'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'Game_start'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_continue_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_restart_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'do_btn_hint'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'paintEvent'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QPaintEvent *, std::false_type>,
+        // method 'closeEvent'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QCloseEvent *, std::false_type>,
+        // method 'on_pushButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_cross_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_color_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_boom_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_help_btn_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void CGameDlg::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<CGameDlg *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->gameToMain(); break;
+        case 1: _t->gameToTheme(); break;
+        case 2: _t->gameToHelp(); break;
+        case 3: _t->on_btn_gameToMain_clicked(); break;
+        case 4: _t->doMainToGame(); break;
+        case 5: _t->doThemeToGame(); break;
+        case 6: _t->on_btn_gameToTheme_clicked(); break;
+        case 7: _t->do_theme_background_change((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 8: _t->do_theme_gem_change((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 9: _t->on_pushButton_stop_clicked(); break;
+        case 10: _t->update_timebar(); break;
+        case 11: _t->Game_start(); break;
+        case 12: _t->on_pushButton_continue_clicked(); break;
+        case 13: _t->on_pushButton_restart_clicked(); break;
+        case 14: _t->do_btn_hint(); break;
+        case 15: _t->paintEvent((*reinterpret_cast< std::add_pointer_t<QPaintEvent*>>(_a[1]))); break;
+        case 16: _t->closeEvent((*reinterpret_cast< std::add_pointer_t<QCloseEvent*>>(_a[1]))); break;
+        case 17: _t->on_pushButton_clicked(); break;
+        case 18: _t->on_pushButton_2_clicked(); break;
+        case 19: _t->on_pushButton_cross_clicked(); break;
+        case 20: _t->on_pushButton_color_clicked(); break;
+        case 21: _t->on_pushButton_boom_clicked(); break;
+        case 22: _t->on_help_btn_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (CGameDlg::*)();
+            if (_t _q_method = &CGameDlg::gameToMain; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (CGameDlg::*)();
+            if (_t _q_method = &CGameDlg::gameToTheme; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (CGameDlg::*)();
+            if (_t _q_method = &CGameDlg::gameToHelp; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 2;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject *CGameDlg::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *CGameDlg::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSCGameDlgENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int CGameDlg::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 23)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 23;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 23)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 23;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void CGameDlg::gameToMain()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void CGameDlg::gameToTheme()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+
+// SIGNAL 2
+void CGameDlg::gameToHelp()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, nullptr);
+}
+QT_WARNING_POP
---
+++ build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/debug/moc_cthemedlg.cpp
@@ -0,0 +1,204 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'cthemedlg.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.7.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../cthemedlg.h"
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'cthemedlg.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.7.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSCThemeDlgENDCLASS_t {};
+constexpr auto qt_meta_stringdata_CLASSCThemeDlgENDCLASS = QtMocHelpers::stringData(
+    "CThemeDlg",
+    "themeToGame",
+    "",
+    "game_theme_background_change",
+    "game_theme_gem_change",
+    "doGameToTheme",
+    "on_btn_themeToGame_clicked",
+    "on_btn_theme_confirm_clicked"
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSCThemeDlgENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       3,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   50,    2, 0x06,    1 /* Public */,
+       3,    1,   51,    2, 0x06,    2 /* Public */,
+       4,    1,   54,    2, 0x06,    4 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       5,    0,   57,    2, 0x08,    6 /* Private */,
+       6,    0,   58,    2, 0x08,    7 /* Private */,
+       7,    0,   59,    2, 0x08,    8 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject CThemeDlg::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSCThemeDlgENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSCThemeDlgENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSCThemeDlgENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<CThemeDlg, std::true_type>,
+        // method 'themeToGame'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'game_theme_background_change'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        // method 'game_theme_gem_change'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        // method 'doGameToTheme'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btn_themeToGame_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btn_theme_confirm_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void CThemeDlg::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<CThemeDlg *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->themeToGame(); break;
+        case 1: _t->game_theme_background_change((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 2: _t->game_theme_gem_change((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 3: _t->doGameToTheme(); break;
+        case 4: _t->on_btn_themeToGame_clicked(); break;
+        case 5: _t->on_btn_theme_confirm_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (CThemeDlg::*)();
+            if (_t _q_method = &CThemeDlg::themeToGame; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (CThemeDlg::*)(QString );
+            if (_t _q_method = &CThemeDlg::game_theme_background_change; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (CThemeDlg::*)(QString );
+            if (_t _q_method = &CThemeDlg::game_theme_gem_change; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 2;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject *CThemeDlg::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *CThemeDlg::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSCThemeDlgENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int CThemeDlg::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 6;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void CThemeDlg::themeToGame()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void CThemeDlg::game_theme_background_change(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+
+// SIGNAL 2
+void CThemeDlg::game_theme_gem_change(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 2, _a);
+}
+QT_WARNING_POP
